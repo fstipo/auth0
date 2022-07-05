f194531efc4bdaa7678ad084e4704aca37728c95
fstipo <fstipanov@gmail.com>
2022-07-05 18:12:53 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useAuth0 } from '@auth0/auth0-react';
 import './App.css';
 
 function App() {
+  const { loginWithPopup, loginWithRedirect, logout, user, isAuthenticated } =
+    useAuth0();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="display-1">Auth0 Authentication</h1>
+      <ul className="list-unstyled">
+        <li>
+          <button className="btn" onClick={loginWithPopup}>
+            Login with Popup
+          </button>
+        </li>
+        <li>
+          <button className="btn" onClick={loginWithRedirect}>
+            Login with Redirect"
+          </button>
+        </li>
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
+        </li>
+      </ul>
+      <h3>User is {isAuthenticated ? 'logged in' : 'not logged in'}</h3>
+      {isAuthenticated && <p>{JSON.stringify(user, null, 2)}</p>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Auth0Provider} from "@auth0/auth0-react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+    domain='f-test.eu.auth0.com'
+    clientId='mbhW7jykatvP9fJdSAclEowb6sWLyG6O'
+    redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
